d2160379e27335479569c220d63351d643a78599
henriSandovalSilva <42452534+henriSandovalSilva@users.noreply.github.com>
2019-05-26 17:19:39 -0300

    Adicionada ScrollView e RaisedButton. Adicionado Padding ao botão e aos TextField

--- lib/main.dart
+++ lib/main.dart
@@ -15,54 +15,81 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Header
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.refresh),
-          )
-        ],
-      ),
-      //Corpo da tela
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(
-            Icons.person_outline,
-            size: 120.0,
-            color: Colors.green,
+        //Header
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.teal,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.refresh),
+            )
+          ],
+        ),
+        //Corpo da tela
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.person,
+                size: 120.0,
+                color: Colors.teal,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(color: Colors.teal),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 25.0,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(color: Colors.teal),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 25.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Calcular',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                      ),
+                    ),
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              Text(
+                'Info',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.teal,
+                  fontSize: 25.0,
+                ),
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Altura (cm)',
-              labelStyle: TextStyle(color: Colors.green),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.green,
-              fontSize: 25.0,
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
